7765846e1dda5ab4f1534b316915d475c5903107
Ester Daniel Ytterbrink <esterdaniel.ytterbrink@eficode.com>
2020-07-06 15:08:52 +0200

    Changes matcher

--- TypeScript/my_service.test.ts
+++ TypeScript/my_service.test.ts
@@ -7,6 +7,6 @@ describe('MyService', ()=>{
     it('rejects invalid SSOToken',() => {
         const service = new MyService(new SingleSignOnRegistry())
         const response = service.handle_request(new Request("Foo", null))
-        expect(response.text).not.toBe("Hello Foo!")
+        expect(response.text).not.toEqual("Hello Foo!")
     })
 })
