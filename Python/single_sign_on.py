b55d3f9373d39f6bbc8ad1f5db60375e08ce07e5
emily <emily@bacheconsulting.com>
2022-03-31 13:55:25 +0200

    sample solution

--- Python/single_sign_on.py
+++ Python/single_sign_on.py
@@ -1,23 +1,47 @@
+import random
+import time
+from dataclasses import dataclass
+
 
 class SingleSignOnRegistry:
-        
-    def register_new_session(self, credentials):
+    """Keeps track of which SSOTokens are currently valid.
+    The real implementation would store these in a database, so
+    for this exercise there are some time.sleep() statements
+    to simulate database access"""
+
+    def __init__(self, authentication_gateway):
+        self._authentication_gateway = authentication_gateway
+        self._valid_tokens = []
+
+    def register_new_session(self, username, password):
         """Returns an instance of SSOToken if the credentials are valid"""
-        pass
+        if self._authentication_gateway.credentials_are_valid(username, password):
+            token = SSOToken()
+            time.sleep(1)
+            self._valid_tokens.append(token)
+            return token
+        return None
 
     def is_valid(self, token):
         """Returns True if the token refers to a current session"""
-        pass
+        time.sleep(1)
+        return token in self._valid_tokens
         
     def unregister(self, token):
         """Remove the given token from current sessions"""
-        pass
+        time.sleep(1)
+        self._valid_tokens.remove(token)
 
 
+@dataclass(frozen=True)
 class SSOToken:
     pass        
 
 
-def credentials_are_valid(credentials):
-    """check the credentials by accessing LDAP or whatever"""
-    return True
+class AuthenticationGateway:
+    def credentials_are_valid(self, username, password):
+        """Check the credentials by accessing LDAP or whatever.
+        This code simulates the effects of that network call
+        """
+        time.sleep(1)
+        return random.choice([True, False])
--- Python/test_my_service.py
+++ Python/test_my_service.py
@@ -1,15 +1,39 @@
-import unittest
+from unittest.mock import MagicMock
 
 from my_service import *
 
 
-class MyServiceTest(unittest.TestCase):
+class SpySingleSignOnRegistry:
 
-    def test_invalidSSOTokenIsRejected(self):
-        service = MyService(None)
-        response = service.handle_request(Request("Foo", None))
-        self.assertNotEqual("hello Foo!", response.text)
-        
-        
-if __name__ == "__main__":
-    unittest.main()
+    def __init__(self, token_is_valid) -> None:
+        self.calls = []
+        self.token_is_valid = token_is_valid
+
+    def is_valid(self, token):
+        self.calls.append("is_valid")
+        return self.token_is_valid
+
+
+def test_valid_sso_token_is_accepted():
+    registry = SpySingleSignOnRegistry(token_is_valid=True)
+    service = MyService(registry)
+    token = SSOToken()
+
+    response = service.handle_request(Request("Foo", token))
+
+    assert "Hello Foo!" == response.text
+    assert registry.calls == ["is_valid"]
+
+
+def test_valid_sso_token_is_accepted_mock_fw():
+    registry = SingleSignOnRegistry(None)
+    registry.is_valid = MagicMock(return_value=True)
+    service = MyService(registry)
+    token = SSOToken()
+
+    response = service.handle_request(Request("Foo", token))
+
+    assert "Hello Foo!" == response.text
+    registry.is_valid.assert_called_once_with(token)
+
+
